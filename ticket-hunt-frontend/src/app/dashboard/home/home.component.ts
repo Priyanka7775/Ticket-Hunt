124dd46fa549d7f67ffadc08d87ad2e84040c013
Rishabh <rishabhgupta7199@gmail.com>
2022-12-23 00:05:02 +0530

    added links to the movies

--- ticket-hunt-frontend/src/app/dashboard/home/home.component.ts
+++ ticket-hunt-frontend/src/app/dashboard/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/model/movie.model';
+import { DataService } from 'src/app/service/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(private dataService: DataService) { }
+
+  movies: Movie[] = [];
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+  
+  getAll(){
+    this.dataService.getAllMovies().subscribe(movie => {
+      this.movies = movie;
+    })
+  }
 
 }
--- ticket-hunt-frontend/src/app/service/data.service.ts
+++ ticket-hunt-frontend/src/app/service/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Movie } from '../model/movie.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,9 @@ import { HttpClient } from '@angular/common/http';
 export class DataService {
 
   constructor(private http: HttpClient) { }
+
+  getAllMovies() {
+    return this.http.get<Array<Movie>>('http://localhost:3000/movies');
+  }
+  
 }
