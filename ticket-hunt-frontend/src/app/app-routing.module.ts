a5e47829b6d7d88db5a2bdb648282e4b156f010e
Rishabh <rishabhgupta7199@gmail.com>
2022-12-22 16:54:36 +0530

    added routes for component in app routing

--- ticket-hunt-frontend/src/app/app-routing.module.ts
+++ ticket-hunt-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './dashboard/home/home.component';
+import { LoginComponent } from './dashboard/login/login.component';
+import { NotFoundComponent } from './dashboard/not-found/not-found.component';
+import { SignupComponent } from './dashboard/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home', 
+    pathMatch: 'full'
+  },
+  {
+    path: '/home',
+    component: HomeComponent, 
+  },
+  {
+    path: '/login', 
+    component: LoginComponent
+  },
+  {
+    path: '/signup', 
+    component: SignupComponent
+  },
+  {
+    path: '/404',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
