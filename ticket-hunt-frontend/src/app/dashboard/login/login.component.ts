8be7e45a9d99173743dd629cd50fb4c54c278868
Rishabh <rishabhgupta7199@gmail.com>
2022-12-22 17:25:51 +0530

    added css and form validation

--- ticket-hunt-frontend/src/app/dashboard/login/login.component.ts
+++ ticket-hunt-frontend/src/app/dashboard/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  
+  constructor(private fb: FormBuilder) { }
+  
+    user = this.fb.group({
+        email: ['', Validators.compose([
+              Validators.required,
+              Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+            ])],
+        password: ['', Validators.required]
+        
+      })
+
+      login(user: FormGroup) {
+        
+      }
 }
