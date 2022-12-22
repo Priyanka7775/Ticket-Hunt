c0291357344dfb8590f4fe7a80bc352943d6d545
Rishabh <rishabhgupta7199@gmail.com>
2022-12-22 17:50:27 +0530

    added validators for the form

--- ticket-hunt-frontend/src/app/dashboard/signup/signup.component.ts
+++ ticket-hunt-frontend/src/app/dashboard/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ValidationErrors, ValidatorFn, Validators, AbstractControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-
+  //Constructor
+  constructor(private fb: FormBuilder) { }
+  
+    customer = this.fb.group({
+      name: ['', Validators.required],
+      age: ['', Validators.required],
+      gender:['', Validators.required],
+      city: ['', Validators.required], 
+      email: ['', Validators.compose([
+            Validators.required,
+            Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+          ])],
+      password: ['', Validators.compose([
+            Validators.required,
+            Validators.pattern(
+              '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d\\W]{8,63}$'
+            ),
+          ])],
+      cnfPassword:['', Validators.required]
+    }, { validator: verifypwd('password', 'cnfPassword') })
+  
+  //  Save Method for the data
+  saveCustomer(customer: FormGroup) {
+    
+  }
 }
+  
+export function verifypwd(pass: string, cnfPass: string): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get(pass)?.value;
+    const cnfPassword = control.get(cnfPass)?.value;
+    if (password != cnfPassword) {
+      const err = { 'noMatch': true };
+      control.get(cnfPass)?.setErrors(err);
+      return err;
+    } 
+    return null;
+  }
+}
+  
