5ee92dfa37ecc4f07d8fe81a7f7770c86173488e
Rishabh <rishabhgupta7199@gmail.com>
2022-12-26 19:54:51 +0530

    added Neo4jRepository implementation

--- recommendation-service/src/main/java/com/niit/tickethunt/repository/EventRepository.java
+++ recommendation-service/src/main/java/com/niit/tickethunt/repository/EventRepository.java
@@ -1,4 +1,8 @@
 package com.niit.tickethunt.repository;
 
-public interface EventRepository {
+import com.niit.tickethunt.domain.Event;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+
+public interface EventRepository extends Neo4jRepository<Event, Long> {
+
 }
