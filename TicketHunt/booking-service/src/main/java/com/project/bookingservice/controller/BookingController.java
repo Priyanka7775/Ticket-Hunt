f63568076f2d700b14472dca177e1552dd1ecfaa
Rohit Narnaware <110964235+rohit-nar7@users.noreply.github.com>
2022-12-26 11:44:40 +0530

    added controller layer with bookSeats booking functionaliy

--- TicketHunt/booking-service/src/main/java/com/project/bookingservice/controller/BookingController.java
+++ TicketHunt/booking-service/src/main/java/com/project/bookingservice/controller/BookingController.java
@@ -1,16 +1,15 @@
 package com.project.bookingservice.controller;
 
 import com.project.bookingservice.domain.Booking;
+import com.project.bookingservice.domain.Seats;
 import com.project.bookingservice.exceptions.EventAlreadyExistException;
+import com.project.bookingservice.exceptions.SeatAlreadyBookedException;
 import com.project.bookingservice.repository.BookingRepository;
 import com.project.bookingservice.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -34,5 +33,18 @@ public class BookingController {
             throw new EventAlreadyExistException();
         }
     }
+
+    @PostMapping("/book/{eventId}/{email}")
+    public  ResponseEntity<?> bookSeats(@PathVariable String eventId, @PathVariable String email, @RequestBody Seats seats) throws SeatAlreadyBookedException {
+
+        try {
+            return new ResponseEntity<>(bookingService.bookSeats(eventId, email, seats), HttpStatus.OK);
+
+        } catch (Exception e) {
+            throw new SeatAlreadyBookedException();
+        }
+
+    }
+
 
 }
