af56acb3b797e2c4c59f4ea8c8ce9bfce387b483
Rohit Narnaware <110964235+rohit-nar7@users.noreply.github.com>
2022-12-26 11:36:12 +0530

    added service layer with book seats implementation

--- TicketHunt/booking-service/src/main/java/com/project/bookingservice/service/BookingService.java
+++ TicketHunt/booking-service/src/main/java/com/project/bookingservice/service/BookingService.java
@@ -1,10 +1,19 @@
 package com.project.bookingservice.service;
 
 import com.project.bookingservice.domain.Booking;
+import com.project.bookingservice.domain.Seats;
 import com.project.bookingservice.exceptions.EventAlreadyExistException;
+import com.project.bookingservice.exceptions.EventNotFoundException;
+import com.project.bookingservice.exceptions.SeatAlreadyBookedException;
+import com.project.bookingservice.exceptions.UserNotFoundException;
 
 public interface BookingService {
 
     Booking addBooking(Booking booking) throws EventAlreadyExistException;
+
+    Booking bookSeats(String eventId, String email, Seats seats) throws EventNotFoundException, UserNotFoundException, SeatAlreadyBookedException;
+
+
+
 
 }
--- TicketHunt/booking-service/src/main/java/com/project/bookingservice/service/BookingServiceImpl.java
+++ TicketHunt/booking-service/src/main/java/com/project/bookingservice/service/BookingServiceImpl.java
@@ -1,10 +1,17 @@
 package com.project.bookingservice.service;
 
 import com.project.bookingservice.domain.Booking;
+import com.project.bookingservice.domain.Seats;
 import com.project.bookingservice.exceptions.EventAlreadyExistException;
+import com.project.bookingservice.exceptions.EventNotFoundException;
+import com.project.bookingservice.exceptions.SeatAlreadyBookedException;
+import com.project.bookingservice.exceptions.UserNotFoundException;
 import com.project.bookingservice.repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class BookingServiceImpl implements BookingService {
 
@@ -28,5 +35,29 @@ public class BookingServiceImpl implements BookingService {
         Booking booking1 = bookingRepository.save(booking);
 
         return booking1;
+    }
+
+
+    @Override
+    public Booking bookSeats(String eventId, String email, Seats seats) throws EventNotFoundException, UserNotFoundException, SeatAlreadyBookedException {
+
+        if (bookingRepository.findById(eventId).isEmpty()) {
+            throw new EventNotFoundException();
+        }
+        System.out.println(seats);
+        Booking booking = bookingRepository.findByEventId(eventId);
+        if (booking.getSeatList() == null) {
+            booking.setSeatList(Arrays.asList(seats));
+        } else {
+            List<Seats> seatsList = booking.getSeatList();
+            if (bookingRepository.findByEventIdAndSeatListSeatNumber(eventId, seats.getSeatNumber()) != null) {
+                System.out.println(bookingRepository.findByEventIdAndSeatListSeatNumber(eventId, seats.getSeatNumber()));
+                throw new SeatAlreadyBookedException();
+            }
+            seatsList.add(seats);
+            booking.setSeatList(seatsList);
+        }
+
+        return bookingRepository.save(booking);
     }
 }
