fac04d91a5b56f370694a449cb877c3209cf9087
Rishabh <rishabhgupta7199@gmail.com>
2022-12-26 19:58:10 +0530

    added abstract method in IEventService

--- recommendation-service/src/main/java/com/niit/tickethunt/service/IEventService.java
+++ recommendation-service/src/main/java/com/niit/tickethunt/service/IEventService.java
@@ -1,4 +1,11 @@
 package com.niit.tickethunt.service;
 
+import com.niit.tickethunt.domain.Event;
+
 public interface IEventService {
+    Event save(Event event);
+
+    Event findById(int Id);
+
+    Event update(Event event);
 }
