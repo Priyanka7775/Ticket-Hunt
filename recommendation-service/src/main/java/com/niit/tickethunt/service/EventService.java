b6b05b93805f2741a9f3be67a6d7cd3535a83965
Rishabh <rishabhgupta7199@gmail.com>
2022-12-26 20:01:42 +0530

    added EventRepository object and Autowired using constructor

--- recommendation-service/src/main/java/com/niit/tickethunt/service/EventService.java
+++ recommendation-service/src/main/java/com/niit/tickethunt/service/EventService.java
@@ -1,8 +1,18 @@
 package com.niit.tickethunt.service;
 
 import com.niit.tickethunt.domain.Event;
+import com.niit.tickethunt.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class EventService implements IEventService{
+
+    private EventRepository eventRepository;
+
+    @Autowired
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
     @Override
     public Event save(Event event) {
         return null;
