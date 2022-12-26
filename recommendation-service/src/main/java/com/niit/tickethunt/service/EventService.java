bdf360c60a36becdb365b3479a4ad36448b143a8
Rishabh <rishabhgupta7199@gmail.com>
2022-12-26 19:59:07 +0530

    added EventService class with implementation of IEventService interface

---
+++ recommendation-service/src/main/java/com/niit/tickethunt/service/EventService.java
@@ -0,0 +1,20 @@
+package com.niit.tickethunt.service;
+
+import com.niit.tickethunt.domain.Event;
+
+public class EventService implements IEventService{
+    @Override
+    public Event save(Event event) {
+        return null;
+    }
+
+    @Override
+    public Event findById(int Id) {
+        return null;
+    }
+
+    @Override
+    public Event update(Event event) {
+        return null;
+    }
+}
