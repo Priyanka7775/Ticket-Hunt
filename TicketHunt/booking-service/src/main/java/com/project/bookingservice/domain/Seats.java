eed70e3aad73236d2e93420b3c95cef35440b09d
Rohit Narnaware <110964235+rohit-nar7@users.noreply.github.com>
2022-12-26 11:27:03 +0530

    added domain classes for booking-service

--- TicketHunt/booking-service/src/main/java/com/project/bookingservice/domain/Seats.java
+++ TicketHunt/booking-service/src/main/java/com/project/bookingservice/domain/Seats.java
@@ -1,4 +1,14 @@
 package com.project.bookingservice.domain;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Data
+@Document(collection = "seats")
 public class Seats {
+    @Id
+    private String seatNumber;
+    private double price;
 }
