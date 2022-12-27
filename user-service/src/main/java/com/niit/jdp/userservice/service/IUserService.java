f3bc92403de1471e8338fae17a05ca0b2c360be2
Mohini <betu1234567@gmail.com>
2022-12-27 12:05:15 +0530

    Added abstract method in the IUserService.java interface

--- user-service/src/main/java/com/niit/jdp/userservice/service/IUserService.java
+++ user-service/src/main/java/com/niit/jdp/userservice/service/IUserService.java
@@ -1,5 +1,17 @@
 package com.niit.jdp.userservice.service;
 
+import com.niit.jdp.userservice.domain.User;
+
+import java.util.List;
+
 public interface IUserService {
+
+    User addBooking(User user) ;
+
+    List<User> getAllUser();
+
+    User findByEmail(String email);
+
+
 
 }
