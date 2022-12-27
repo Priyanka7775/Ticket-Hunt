2e7f0f4acce6bd367944d9a6a0548a3e2a016f82
Mohini <betu1234567@gmail.com>
2022-12-27 12:07:23 +0530

    created a UserServiceImpl class

---
+++ user-service/src/main/java/com/niit/jdp/userservice/service/UserServiceImpl.java
@@ -0,0 +1,10 @@
+/*
+ * Author Name: Mohini
+ * Date: 12/27/2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp.userservice.service;
+
+public class UserServiceImpl {
+    
+}
