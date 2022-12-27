e8a99427d9e6171cb2f6b71c2df8cad36814da44
Mohini <betu1234567@gmail.com>
2022-12-27 11:31:38 +0530

    add some required annotation in user class

--- user-service/src/main/java/com/niit/jdp/userservice/domain/User.java
+++ user-service/src/main/java/com/niit/jdp/userservice/domain/User.java
@@ -5,18 +5,40 @@
  */
 package com.niit.jdp.userservice.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Document
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Getter
+    @Setter
     private String name;
+    @Id
+    @Getter
+    @Setter
     private String email;
+    @Getter
+    @Setter
     private String password;
+    @Getter
+    @Setter
     private String city;
+    @Getter
+    @Setter
     private String role;
+    @Getter
+    @Setter
     private List<String> interest;
+    @Getter
+    @Setter
     private long phone;
 
 
