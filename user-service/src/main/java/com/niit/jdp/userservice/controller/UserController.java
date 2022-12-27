6a58d5604ba36b87810fcad592ca397372990f89
Mohini <betu1234567@gmail.com>
2022-12-27 17:46:04 +0530

    created UserController.java in the Controller package

---
+++ user-service/src/main/java/com/niit/jdp/userservice/controller/UserController.java
@@ -0,0 +1,12 @@
+/*
+ * Author Name: Mohini
+ * Date: 12/27/2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp.userservice.controller;
+
+import com.niit.jdp.userservice.service.IUserService;
+
+public class UserController {
+    private IUserService iUserService;
+}
