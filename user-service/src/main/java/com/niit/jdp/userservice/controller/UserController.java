3d06d2503628460a56a3ad03a76531708b4d64b1
Mohini <betu1234567@gmail.com>
2022-12-27 18:17:22 +0530

    created controller layer

--- user-service/src/main/java/com/niit/jdp/userservice/controller/UserController.java
+++ user-service/src/main/java/com/niit/jdp/userservice/controller/UserController.java
@@ -5,8 +5,59 @@
  */
 package com.niit.jdp.userservice.controller;
 
+import com.niit.jdp.userservice.domain.User;
+import com.niit.jdp.userservice.exception.UserAlreadyExistsException;
+import com.niit.jdp.userservice.exception.UserNotFoundException;
 import com.niit.jdp.userservice.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/userdata/")
+@CrossOrigin("*")
 public class UserController {
     private IUserService iUserService;
-}
+@Autowired
+    public UserController(IUserService iUserService) {
+        this.iUserService = iUserService;
+    }
+    @PostMapping("/adduser")
+    public ResponseEntity<?> add(@RequestBody User user) throws UserAlreadyExistsException {
+            ResponseEntity responseEntity = null;
+            try{
+                responseEntity = new ResponseEntity<>(iUserService.addUser(user), HttpStatus.CREATED);
+            }catch(UserAlreadyExistsException e){
+                throw new UserAlreadyExistsException();
+            }catch(Exception e){
+                responseEntity = new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            return responseEntity;
+        }
+    @GetMapping("/alluser")
+    public ResponseEntity<?> fetchAllUser(){
+        ResponseEntity responseEntity = null;
+        try{
+            responseEntity = new ResponseEntity(iUserService.getAllUser(), HttpStatus.OK);
+        }catch (Exception exception){
+            responseEntity = new ResponseEntity(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return responseEntity;
+    }
+    @GetMapping("/email/{email}")
+    public ResponseEntity<?> fetchUseByEmail(@PathVariable String email) throws UserNotFoundException {
+            ResponseEntity responseEntity = null;
+            try{
+                responseEntity = new ResponseEntity(iUserService.findByEmail(email),HttpStatus.NOT_FOUND);
+            }catch(UserNotFoundException exception){
+                throw new UserNotFoundException();}
+            catch(Exception e){
+                responseEntity = new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            return responseEntity;
+        }
+
+    }
+
+
