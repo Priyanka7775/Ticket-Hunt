d03002a9e88ce3cfafaf0383ec5641df61e68409
Rishabh <rishabhgupta7199@gmail.com>
2022-12-27 21:27:36 +0530

    added User CURD related APIs for neo4j

--- recommendation-service/src/main/java/com/niit/tickethunt/controller/GlobalController.java
+++ recommendation-service/src/main/java/com/niit/tickethunt/controller/GlobalController.java
@@ -1,7 +1,9 @@
 package com.niit.tickethunt.controller;
 
 import com.niit.tickethunt.domain.Event;
+import com.niit.tickethunt.domain.User;
 import com.niit.tickethunt.service.EventService;
+import com.niit.tickethunt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,24 +12,47 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/v4/")
 public class GlobalController {
-    private EventService eventService;
+    private final EventService eventService;
+    private final UserService userService;
+
     @Autowired
-    public GlobalController(EventService eventService) {
+    public GlobalController(EventService eventService, UserService userService) {
         this.eventService = eventService;
+        this.userService = userService;
     }
 
+    /* Event CRUD related APIs */
     @GetMapping("events")
-    public ResponseEntity<?> getAllEvents(){
+    public ResponseEntity<?> getAllEvents() {
         return new ResponseEntity<>(eventService.getAll(), HttpStatus.OK);
     }
 
     @PostMapping("event/save")
-    public ResponseEntity<?> saveEvent(@RequestBody Event event){
+    public ResponseEntity<?> saveEvent(@RequestBody Event event) {
         return new ResponseEntity<>(eventService.save(event), HttpStatus.CREATED);
     }
 
     @GetMapping("event/{id}")
-    public ResponseEntity<?> getById(@PathVariable int id){
+    public ResponseEntity<?> getById(@PathVariable int id) {
         return new ResponseEntity<>(eventService.findById(id), HttpStatus.OK);
     }
+
+
+    /*  User CURD related APIs */
+
+    @GetMapping("users")
+    public ResponseEntity<?> getAllUsers() {
+        return new ResponseEntity<>(userService.getAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("user/save")
+    public ResponseEntity<?> saveUser(@RequestBody User user) {
+        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping("user/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable int id) {
+        return new ResponseEntity<>(userService.findById(id), HttpStatus.OK);
+    }
+
 }
