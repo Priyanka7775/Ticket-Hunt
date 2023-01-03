df46acb363388917a34abf4a84d209ea23975420
Ranjan Sengupta <ranjan_201700181@smit.smu.edu.in>
2023-01-03 07:34:13 +0530

    slight modifications to authentication

--- authentication-services/src/main/java/com/preproject/authentication/model/User.java
+++ authentication-services/src/main/java/com/preproject/authentication/model/User.java
@@ -1,50 +1,23 @@
 package com.preproject.authentication.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
-//contains the data of the application. A data can be a single object or a collection of objects.
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class User {
 
     @Id
     private String email;
     private String password;
     private String role;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", role='" + role + '\'' +
-                '}';
-    }
 }
+
