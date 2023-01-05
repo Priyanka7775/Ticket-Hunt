f9b24c9823fb7dbe8c84d399a9b5979fa2469bc0
Rishabh <rishabhgupta7199@gmail.com>
2023-01-05 22:58:36 +0530

    added seats and booking class

--- recommendation-service/src/main/java/com/niit/tickethunt/domain/Booking.java
+++ recommendation-service/src/main/java/com/niit/tickethunt/domain/Booking.java
@@ -1,9 +1,22 @@
 package com.niit.tickethunt.domain;
 
 
-import org.springframework.data.neo4j.core.schema.Relationship;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
 
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Booking {
-
+    @Id
+    private Long id;
+    private List<Seats> seatList;
+    private int totalSeats;
+    private String date;
+    private String payment;
 }
---
+++ recommendation-service/src/main/java/com/niit/tickethunt/domain/Seats.java
@@ -0,0 +1,16 @@
+package com.niit.tickethunt.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Seats {
+    private String seatNumber;
+    private double price;
+    private Date dateOfBooking;
+}
