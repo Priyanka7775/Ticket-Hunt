1a16581602f6c334308436d905d85887385d11df
Rishabh <rishabhgupta7199@gmail.com>
2023-01-05 22:51:53 +0530

    added booked relation field

--- recommendation-service/src/main/java/com/niit/tickethunt/domain/Booking.java
+++ recommendation-service/src/main/java/com/niit/tickethunt/domain/Booking.java
@@ -1,4 +1,8 @@
 package com.niit.tickethunt.domain;
 
+
+import org.springframework.data.neo4j.core.schema.Relationship;
+
 public class Booking {
+
 }
--- recommendation-service/src/main/java/com/niit/tickethunt/domain/User.java
+++ recommendation-service/src/main/java/com/niit/tickethunt/domain/User.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
 
 import java.util.List;
 @Node
@@ -21,4 +22,7 @@ public class User {
    private String role;
    private List<String> interest;
    private long phone;
+
+   @Relationship(type = "BOOKED", direction = Relationship.Direction.OUTGOING)
+   private Booking booking;
 }
