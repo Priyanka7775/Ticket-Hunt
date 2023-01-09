7faf591f0863dffff077e4257be3ee844165e282
AMAN GUPTA <aman.gupta4692@gmail.com>
2023-01-09 18:47:08 +0530

    updated add event code

--- tickethunt-webapp/src/app/model/event.model.ts
+++ tickethunt-webapp/src/app/model/event.model.ts
@@ -1,14 +1,15 @@
 import { FileHandle } from "./file-handle.model";
 
 export type EventData = {
-    eventId?: String, 
-    email?:String,
+    eventId?: String,
+    email?: String,
     eventName?: String,
-    date?: String, 
-    time?: String, 
-    venue?: String, 
+    date?: String,
+    time?: String,
+    venue?: String,
     image?: any,
-    organizerName?: String, 
+    organizerName?: String,
     totalSeat?: Number,
+    eventType?: string,
     // price?: Number,
 }
