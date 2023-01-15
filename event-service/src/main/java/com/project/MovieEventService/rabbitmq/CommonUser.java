d217b45d298ef14f55554d3ae71a711b7c1dd0f6
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2023-01-15 18:48:40 +0530

    did changes in Movie-event services

--- event-service/src/main/java/com/project/MovieEventService/rabbitmq/CommonUser.java
+++ event-service/src/main/java/com/project/MovieEventService/rabbitmq/CommonUser.java
@@ -18,9 +18,9 @@ public class CommonUser {
     private String eventType;
     private String description;
     private int price;
-    private double rating;
+    private String rating;
 
-    public CommonUser(String eventId, String email, String eventName, String organizerName, String date, String time, String venue, byte[] image, int totalSeat, String eventType, String description, int price, double rating) {
+    public CommonUser(String eventId, String email, String eventName, String organizerName, String date, String time, String venue, byte[] image, int totalSeat, String eventType, String description, int price, String rating) {
         this.eventId = eventId;
         this.email = email;
         this.eventName = eventName;
