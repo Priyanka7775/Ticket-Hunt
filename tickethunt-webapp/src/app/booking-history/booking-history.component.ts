8c9c62f1853a7c89464d546d082397b387ea21f5
Rohit Narnaware <110964235+rohit-nar7@users.noreply.github.com>
2023-01-16 15:16:52 +0530

    added alert after cancelling seats

--- tickethunt-webapp/src/app/booking-history/booking-history.component.ts
+++ tickethunt-webapp/src/app/booking-history/booking-history.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 import { Bookings } from '../model/bookings';
 import { BookingServiceService } from '../service/booking.service';
 
@@ -40,7 +41,13 @@ getData(){
 
 delete(seat: any, eventId: any) {
 
-  
+  Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Successfully cancelled=> '+seat,
+    showConfirmButton: false,
+    timer: 4500
+  })
 /*   alert("successfully canceled seat =>"+seat)
  */  window.location.reload();
   this.bookingsService.cancelSeats(seat, eventId).subscribe(
