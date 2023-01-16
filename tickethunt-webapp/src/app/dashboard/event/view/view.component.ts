7c2c0e8f2be7817164191ff4d3848e43ed0f283d
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2023-01-16 16:40:49 +0530

    added sweet alert

--- tickethunt-webapp/src/app/dashboard/event/view/view.component.ts
+++ tickethunt-webapp/src/app/dashboard/event/view/view.component.ts
@@ -63,31 +63,29 @@ export class ViewEventComponent implements OnInit {
   }
 
   deleteNote(eventId: any) {
-    this.dataService.deletePost(eventId).subscribe(data => {
-      console.log("test delete")
-      Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire(
-            'Deleted!',
-            'Your file has been deleted.',
-            'success'
-           
-          )
-          location.reload();
-        }
-       
-      })
-      
-       
-    });
+    
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.dataService.deletePost(eventId).subscribe(data => {
+          console.log("test delete")
+          // window.location.reload();
+        });
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+        location.reload();
+      }
+    })
     
   }
  
