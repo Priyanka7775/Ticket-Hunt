e7ff1a027cf0d544b481261a2bdd082ee5e0671c
AMAN GUPTA <aman.gupta4692@gmail.com>
2023-01-17 17:10:53 +0530

    email service added again

--- authentication/src/main/java/com/project/authentication/service/UserServiceImpl.java
+++ authentication/src/main/java/com/project/authentication/service/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.project.authentication.service;
 import com.project.authentication.domain.User;
 import com.project.authentication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +15,10 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+
     @Override
     public List<User> getUser() {
         return userRepository.findAll();
@@ -21,20 +27,28 @@ public class UserServiceImpl implements UserService {
     @Override
     public User addUser(User user) {
         return userRepository.save(user);
-
     }
 
     @Override
     public User authCheck(String email, String pass) {
-        if (userRepository.findById(email).isPresent()) {
-            User user = userRepository.findById(email).get();
-            if (user.getPassword().equals(pass)) {
-                return user;
-            } else {
-                return null;
-            }
-        } else {
-            return null;
-        }
+//        if (userRepository.findById(email).isPresent()) {
+//            User user = userRepository.findById(email).get();
+//            if (user.getPassword().equals(pass)) {
+//                return user;
+//            } else {
+//                return null;
+//            }
+//        } else {
+//            return null;
+//        }
+        User user = userRepository.findById(email).get();
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(email);
+        msg.setSubject("Login");
+        msg.setText("You have successfully logged in!!!!.");
+        javaMailSender.send(msg);
+        return user;
     }
+
 }
