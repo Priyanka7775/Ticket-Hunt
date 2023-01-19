6a1cff2adc8bcd23a622bcb682b6abc064fb3ebb
AMAN GUPTA <aman.gupta4692@gmail.com>
2023-01-19 17:42:46 +0530

    updated app config file

--- APIGateway/src/main/java/com/project/APIGateway/config/AppConfig.java
+++ APIGateway/src/main/java/com/project/APIGateway/config/AppConfig.java
@@ -33,8 +33,8 @@ public class AppConfig {
                 )
                 .route(p -> p
                         .path("/userData/**")
-                        .uri("http://localhost:8082/"))
-
+                        .uri("http://localhost:8082/")
+                )
                 .build();
     }
 }
