ca5abb722677d0efaab8deec50230d9bd3df3dd5
AMAN GUPTA <aman.gupta4692@gmail.com>
2023-01-19 17:41:01 +0530

    updated API gateway config file

--- APIGateway/src/main/java/com/project/APIGateway/config/AppConfig.java
+++ APIGateway/src/main/java/com/project/APIGateway/config/AppConfig.java
@@ -13,7 +13,28 @@ public class AppConfig {
         return builder.routes().
                 route(p -> p
                         .path("/niit/auth/**")
-                        .uri("http://localhost:8084/"))
+                        .uri("http://localhost:8084/")
+                )
+                .route(p -> p
+                        .path("/bookings/**")
+                        .uri("http://localhost:8081/")
+                )
+                .route(p -> p
+                        .path("/eventData/**")
+                        .uri("http://localhost:8088/")
+                )
+                .route(p -> p
+                        .path("/payment/**")
+                        .uri("http://localhost:8085/")
+                )
+                .route(p -> p
+                        .path("/api/v4/**")
+                        .uri("http://localhost:8086/")
+                )
+                .route(p -> p
+                        .path("/userData/**")
+                        .uri("http://localhost:8082/"))
+
                 .build();
     }
 }
--- payment-service/src/main/java/com/project/paymentservice/controller/RazorPayController.java
+++ payment-service/src/main/java/com/project/paymentservice/controller/RazorPayController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigInteger;
 
 @RestController
-@RequestMapping("/v1/")
+@RequestMapping("/payment/")
 @CrossOrigin(origins = "http://localhost:4200")
 public class RazorPayController {
 
