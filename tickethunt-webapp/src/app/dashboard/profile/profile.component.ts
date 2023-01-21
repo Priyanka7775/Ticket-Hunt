66111937422ce7c4874cda531a69d686443dae08
Rishabh <rishabhgupta7199@gmail.com>
2023-01-22 01:39:48 +0530

    added getUser method for profile

--- tickethunt-webapp/src/app/dashboard/profile/profile.component.ts
+++ tickethunt-webapp/src/app/dashboard/profile/profile.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/model/user.model';
+import { SignupService } from 'src/app/service/signup.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
+  profile: User[] = [];
 
+  constructor(private signupService: SignupService) {}
+  // Get user info when loggin
+
+  getUser(email: string) {
+    this.signupService.getUser(email).subscribe((x) => (this.profile = x));
+  }
 }
--- tickethunt-webapp/src/app/service/signup.service.ts
+++ tickethunt-webapp/src/app/service/signup.service.ts
@@ -1,12 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { User } from '../model/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SignupService {
-
-  constructor(private http: HttpClient) { }
-  post(user: User) { return this.http.post('http://localhost:8082/userData/commonUser', user) }
+  constructor(private http: HttpClient) {}
+  post(user: User) {
+    return this.http.post('http://localhost:8082/userData/commonUser', user);
+  }
+  getUser(email: string): Observable<Array<User>> {
+    return this.http.get<Array<User>>(
+      'http://localhost:8082/userData/commonUser'
+    );
+  }
 }
