0a70136623d5d7a94c7e4bd27c7c0ed9c050d439
Rishabh <rishabhgupta7199@gmail.com>
2023-01-25 18:47:23 +0530

    updated ui of detail component

--- tickethunt-webapp/src/app/detail/detail.component.ts
+++ tickethunt-webapp/src/app/detail/detail.component.ts
@@ -1,43 +1,57 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { EventData } from '../model/event';
 import { DataService } from '../service/data.service';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.css'],
 })
 export class DetailComponent {
-  constructor(private activatedRoute: ActivatedRoute, private dataService: DataService, private router: Router) { }
-  event: any = {}
+  
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private dataService: DataService,
+    private router: Router
+  ) {}
+  event: any = {};
 
   @Input()
   events: any;
 
+  movies: EventData[] = [];
+
   ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe(
-
-      params => {
-        let id = params.get("id");
-        console.log(id);
-        this.dataService.getEvent1(id).subscribe(data => {
-          console.log(data);
-          this.event = data;
-          sessionStorage.setItem("eventName", this.event.eventName);
-          sessionStorage.setItem("eventId", this.event.eventId);
-          sessionStorage.setItem("venue", this.event.venue);
-          sessionStorage.setItem("date", this.event.date);
-          sessionStorage.setItem("totalSeats", this.event.totalSeats);
-          sessionStorage.setItem("time", this.event.time);
-          sessionStorage.setItem("price", this.event.price)
-          console.log(data);
-        })
-      }
-    )
-
+    this.activatedRoute.paramMap.subscribe((params) => {
+      let id = params.get('id');
+      console.log(id);
+      this.dataService.getEvent1(id).subscribe((data) => {
+        console.log(data);
+        this.event = data;
+        sessionStorage.setItem('eventName', this.event.eventName);
+        sessionStorage.setItem('eventId', this.event.eventId);
+        sessionStorage.setItem('venue', this.event.venue);
+        sessionStorage.setItem('date', this.event.date);
+        sessionStorage.setItem('totalSeats', this.event.totalSeats);
+        sessionStorage.setItem('time', this.event.time);
+        sessionStorage.setItem('price', this.event.price);
+      });
+      this.getRecomendation(id);
+    });
   }
+
+  getRecomendation(id:any) {
+    this.dataService.getAllEvent().subscribe((value) => {
+      this.movies = value.filter(x=>x.eventId != id);
+    });
+  }
+
+  navigateToMovieDetail(eventId: any) {
+    this.router.navigate(['detail', eventId]);
+  }
+
   book() {
     this.router.navigate(['booking/' + this.event.eventId]);
   }
-
 }
