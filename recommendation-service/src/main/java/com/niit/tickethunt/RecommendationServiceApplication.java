f99cc095d298e2069b7c9049693f5a63840ef2d4
Rishabh <rishabhgupta7199@gmail.com>
2023-01-30 17:30:29 +0530

    added SwaggerConfig file for api documentation

--- recommendation-service/src/main/java/com/niit/tickethunt/RecommendationServiceApplication.java
+++ recommendation-service/src/main/java/com/niit/tickethunt/RecommendationServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableNeo4jRepositories
 @EnableTransactionManagement
 @EnableEurekaClient
+@EnableSwagger2
 public class RecommendationServiceApplication {
 
 	public static void main(String[] args) {
